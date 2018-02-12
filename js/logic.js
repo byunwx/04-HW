a5e64b2850e950aa300535fac7a70e34f30ffd8b
Woojoo Byun <woojoobyun@Woojoos-MacBook-Pro.local>
2018-02-12 12:56:54 -0500

    fixed bug

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-const characterDefult=[
+var characterDefult=[
   kenobi={
     id: "kenobi",
     name: "Obi-wan Kenobi",
@@ -46,10 +46,75 @@ const characterDefult=[
     status:"alive"
   }
 ];
-let character= characterDefult;
+
+var character=characterDefult.slice(0);
 var userChar="";
 var enemyDefined="";
 
+function start(){
+  console.log(characterDefult);
+  character=[
+    kenobi={
+      id: "kenobi",
+      name: "Obi-wan Kenobi",
+      hp: 120,
+      ap: 8,
+      cp: 10,
+      index:0,
+      status:"alive"
+    },
+    skywalker={
+      id: "skywalker",
+      name: "Luke Skywalker",
+      hp: 100,
+      ap: 8,
+      cp: 5,
+      index:1,
+      status:"alive"
+    },
+    sidious={
+      id: "sidious",
+      name: "Darth Sidious",
+      hp: 140,
+      ap: 8,
+      cp: 20,
+      index:2,
+      status:"alive"
+    },
+    maul={
+      id: "maul",
+      name: "Darth Maul",
+      hp: 150,
+      ap: 8,
+      cp: 30,
+      index:3,
+      status:"alive"
+    },
+    yoda={
+      id: "yoda",
+      name: "Master Yoda",
+      hp: 150,
+      ap: 20,
+      cp: 25,
+      index:4,
+      status:"alive"
+    }
+  ];
+
+  userChar="";
+  enemyDefined="";
+  $("#msg").empty();
+  $("#message").empty();
+  $("#enemy").empty();
+  $("#available").empty();
+  $("#chosen").empty();
+  $("#buttonArea").empty();
+  for (var i = 0; i < character.length; i++) {
+    createCharacter("#choice", character[i], "red");
+  }
+  document.getElementById("message").innerHTML="";
+  choice(".characterBox");
+};
 function createCharacter(id, char, color){
   var newID= char.id
   var newDiv = document.createElement("div");
@@ -72,32 +137,13 @@ function createCharacter(id, char, color){
 };
 
 
-function start(){
-  console.log(characterDefult);
-  character= characterDefult;
-  userChar="";
-  enemyDefined="";
-  $("#msg").empty();
-  $("#message").empty();
-  $("#enemy").empty();
-  $("#available").empty();
-  $("#chosen").empty();
-  $("#buttonArea").empty();
-  for (var i = 0; i < character.length; i++) {
-    createCharacter("#choice", character[i], "red");
-  }
-  document.getElementById("message").innerHTML="";
-  choice(".characterBox");
-};
-
-
 function choice(ind){
   $(ind).click(function(){
     var newID = $(this).attr("index");
   $("#choice").empty();
   userChar= newID;
   console.log("heroChosen: "+character[userChar].id);
-  enemyChoice(newID);
+  enemyChoice();
   });
 }
 
@@ -109,23 +155,28 @@ function enemyChoice(){
       deadcount+=1;
     };
   };
+  if (character.length==deadcount) {
+  document.getElementById("message").innerHTML="YOU HAVE WON THE GAME!PRESS RESET TO START NEWGAME!";
+  $("#msg").html("<button class='btn bg-primary btn-lg'>Reset</button>");
+  $(".bg-primary").click(function(){
+    start();
+  });
+  }
+  else{
   console.log(deadcount);
   for (var i = 0; i < character.length; i++) {
     if(character[i].index==userChar){
       createCharacter("#chosen", character[i], "green");
     }else if(character[i].status=="alive"){
       createCharacter("#available", character [i], "red");
-    }else if (character.length==deadcount) {
-      document.getElementById("message").innerHTML="YOU HAVE WON THE GAME!PRESS RESET TO START NEWGAME!";
-      $("#msg").html("<button class='btn bg-primary btn-lg'>Reset</button>");
-      $("#msg").click(function(){
-        start();
-      });
     }else{
       console.log("dead: "+character[i].name);
     }
+
   };
+
   enemy();
+  };
 };
 
 
@@ -148,9 +199,9 @@ function moveToAttack(){
   $("#buttonArea").html("<button id='attackBtn' class='btn bg-danger btn-lg'>ATTACK!</button>");
   for (var i = 0; i < character.length; i++) {
     if(character[i].index==enemyDefined){
-      createCharacter("#enemy", characterDefult[i], "red");
+      createCharacter("#enemy", character[i], "red");
     }else if(character[i].index!=userChar && character[i].index!=enemyDefined && character[i].status=="alive"){
-      createCharacter("#available", characterDefult[i], "yellow");
+      createCharacter("#available", character[i], "yellow");
     }else{
       console.log("Dead Enemy: "+character[i].id);
     }
@@ -176,7 +227,7 @@ function attackReady(){
     $("#available").empty();
     $("#chosen").empty();
     $("#buttonArea").empty();
-    character= characterDefult;
+
     start();
 
   }else if(character[enemyDefined].hp<0){
